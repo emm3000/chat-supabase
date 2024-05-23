88d983ae99339debc045b8df93b141ba48a0d027
Edgardo Muñoz <edgardo.emm20@gmail.com>
2024-05-23 02:11:39 -0500

    add second route #11

--- app/src/main/java/com/emm/bingo/BingoApp.kt
+++ app/src/main/java/com/emm/bingo/BingoApp.kt
@@ -13,7 +13,7 @@ class BingoApp: Application() {
         super.onCreate()
         startKoin {
             androidLogger() // fake route
-            androidContext(this@BingoApp) // second route // aca // tmr
+            androidContext(this@BingoApp) // second route // aca // tmr // no funciona
             modules(
                 sharedModule,
                 loginModule
